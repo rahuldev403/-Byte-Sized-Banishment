d419e6742c0e97b46b5db24359cf30777c3e84a9
Rahul Swain <level432520537352822@gmail.com>
2025-08-20 10:32:25 +0530

    refactor: Standardize quotes and update token expiration to 1 day in token model

--- byte-sized-banishment/server/models/tokenModel.js
+++ byte-sized-banishment/server/models/tokenModel.js
@@ -1,21 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const tokenSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
-    token: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        expires: 3600 // 1 hour
-    }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 86400, // 1 day
+  },
 });
 
-const Token = mongoose.model('Token', tokenSchema);
+const Token = mongoose.model("Token", tokenSchema);
 export default Token;
